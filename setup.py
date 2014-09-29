5e21c4191010cdb362addffcc2e6b1373b1cf7ee
Rihards Krišlauks <rihizs@gmail.com>
2014-09-30 02:47:53 +0300

    version++

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='LatvianStemmer',
-    version='1.0.0',
+    version='1.0.1',
     description='Light stemmer for Latvian.',
     py_modules=['LatvianStemmer'],
     author='Rihards Krišlauks',
